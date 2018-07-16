57d8c8485d95752ec2ce20539bf7406b416b821d
Harmen Backhaus <harmen.backhaus@pc033.kega.local>
2018-07-16 14:36:03 +0200

    +: second test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+# last test
 # This script will ssh into the first webserver and do an admin deployment, then a web deployment on all web servers.
 # This script can only be executed on the bastion host from the folder aws-ansible.
 
